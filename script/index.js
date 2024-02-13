75257092a01a9d61ffb38bc38dd1d35d57fa6fcb
mozzammelrobi <mozzammelfb@gmail.com>
2024-02-14 04:12:46 +0600

    home screen page to plagroung page go throw button click done and for the playground get random alphabet aalso done

--- script/index.js
+++ script/index.js
@@ -1,1 +1,28 @@
-console.log('test ')
+// function play(){
+//     /**
+//      * step-1: hide the home screen. To hide the screen add the hidden class to the home section.
+//      * step-2: show the playgroun. 
+//      */
+//     // step-1
+//     const homesection = document.getElementById('home-screen')
+//     homesection.classList.add('hidden')
+
+//     // step-2
+//     const playground = document.getElementById('play-ground')
+//     playground.classList.remove('hidden')
+//     // console.log(playground.classList)
+// }
+
+
+function continueGame(){
+    // step-1: generate a randome alphabet
+    const alphabet = getARandomAlphabet()
+    console.log('random alphabet', alphabet)
+}
+
+function play(){
+    hideElementById('home-screen')
+    showElementById('play-ground')
+    continueGame()
+}
+
